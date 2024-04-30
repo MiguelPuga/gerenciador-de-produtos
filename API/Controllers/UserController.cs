029b2dd13bfc762c66cfd30feafe5c9b1872ad3e
Miguel Puga <miguel.puga@brpartners.com.br>
2024-04-30 18:42:15 -0300

    Update api

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿namespace API;
+
+using Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController
+{
+    private readonly IMediator _mediator;
+    private readonly UserRepository _repository;
+
+    UserController(IMediator mediator, UserRepository repository)
+    {
+        _mediator = mediator;
+        _repository = repository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateUserRequest command)
+    {
+        var response = await _mediator.Send(command);
+        return new OkObjectResult(response);
+    }
+}
